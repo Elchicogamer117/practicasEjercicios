318bdffde84783affefa42e50b88eeda9b10671a
Cristian <cris18002011@hotmail.com>
2022-04-23 15:47:04 -0500

    Ejercicio promises

--- nodejs/async/promises.js
+++ nodejs/async/promises.js
@@ -1,20 +1,44 @@
-function hola(name, myCallback){
-      setTimeout(() => {  
-            console.log(`Hola ${name} 🖐`)
-            myCallback(name)
-      }, 2000)
+function hola(name){
+      return new Promise((resolve, reject) => {
+            setTimeout(() => {  
+                  console.log(`Hola ${name} 🖐`)
+                  resolve(name)
+            }, 2000)
+      })
+      
 }
 
-function hablar(callCallback){
-      setTimeout(() =>{
-            console.log('Te lo he contado alguna vez...')
-            callCallback()
-      },1500)
+function hablar(name){
+      return new Promise((resolve, reject) => {
+            setTimeout(() => {  
+                  console.log(`Te lo he contado alguna vez...`)
+                  resolve(name)
+                  //reject('No se pudo procesar la conversacion')
+            }, 1500)
+      })
 }
 
-function adios(name, otroCallback){
-      setTimeout(() => {  
-            console.log(`Adios ${name} 🖐`)
-            otroCallback()
-      }, 1000)
+function adios(name){
+      return new Promise((resolve, reject) => {
+            setTimeout(() => {  
+                  console.log(`Adios ${name} 🖐`)
+                  resolve()
+            }, 1000)
+      })
 }
+
+//*----------- main.js ------------
+
+console.log('Iniciando proceso...')
+hola('Cristian')
+      .then(hablar)
+      .then(hablar)
+      .then(hablar)
+      .then(adios)
+      .then(() => {
+            console.log('Terminamos el proceso...')
+      })
+      .catch(error => {
+            console.error('Ha habido un error:');
+            console.error(error);
+      })
