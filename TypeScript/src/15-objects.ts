fa6f56785c53cedb8b8e7ab7077eb60e6f676417
Cristian Flores <cris18002011@hotmail.com>
2022-06-02 15:56:12 -0500

    Reutilizando tipado de objetos

---
+++ TypeScript/src/15-objects.ts
@@ -0,0 +1,37 @@
+(() => {
+
+  type Sizes =  'S' | 'M' | 'L' | 'XL';
+  type Product = {
+    title: string,
+    createAt: Date,
+    stock: number,
+    size?: Sizes
+  }
+  const product:Product[] = [];
+
+  const addProduct = (data: Product) => {
+    product.push(data)
+  }
+
+  addProduct({
+    title: 'Halo CE',
+    createAt: new Date(2001, 11, 15),
+    stock: 343
+  });
+  addProduct({
+    title: 'Halo shirt ',
+    createAt: new Date(2001, 12, 10),
+    stock: 117,
+    size:'XL'
+  });
+   console.log(product)
+  //  product.push('Halo 2');
+  //  product.push(true);
+  product.push({
+    title: 'Halo 2 ',
+    createAt: new Date(2004, 11, 9),
+    stock: 216,
+  });
+  console.log(product)
+
+})();
