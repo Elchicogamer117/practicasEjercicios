e3b8843a2d0bd065a750edd215ac2afb7241bc83
Cristian Flores <cris18002011@hotmail.com>
2022-06-02 23:15:02 -0500

    Consumiendo api con promesas

---
+++ asincronismojs/src/promise/challenge.js
@@ -0,0 +1,16 @@
+const fetchData = require('../utils/fetchData');
+let apiRickAndMorty = 'https://rickandmortyapi.com/api/character/';
+
+fetchData(apiRickAndMorty)
+    .then(data => {
+        console.log(data.info.count) 
+        return fetchData(`${apiRickAndMorty}${data.results[0].id}`)
+    })
+    .then(data => {
+        console.log(data.name)
+        return fetchData(data.origin.url)
+    })
+    .then(data => {
+        console.log(data.dimension)
+    })
+    .catch(err => console.error(err));
