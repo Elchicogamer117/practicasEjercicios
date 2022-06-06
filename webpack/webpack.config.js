e098d08e4180d45ea1251815d2e823fb9d3bc1b6
Cristian Flores <cris18002011@hotmail.com>
2022-06-06 15:59:36 -0500

    webpack: Optimizando archivos (hashes, compresión y minificación)

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js',
+      filename: '[name].[contenthash].js',
       assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
@@ -39,13 +41,9 @@ module.exports = {
           use: {
             loader: "url-loader",
             options: {
-              //* limit => limite de tamaño
               limit: 10000,
-              //* Mimetype => tipo de dato
               mimetype: "application/font-woff",
-              //* name => nombre de salida
-              name: "[name].[ext]",
-              //* outputPath => donde se va a guardar en la carpeta final
+              name: "[name].[contenthash].[ext]",
               outputPath: "./assets/fonts/",
               publicPath: "./assets/fonts/",
               esModule: false,
@@ -60,7 +58,9 @@ module.exports = {
           template: './public/index.html', 
           filename: './index.html' 
       }),
-      new MiniCssExtractPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'assets/[name].[contenthash].css'
+      }),
       new CopyPlugin({
         patterns: [
           {
@@ -69,5 +69,12 @@ module.exports = {
           }
         ]
       }),
-  ]
+  ],
+  optimization: {
+    minimize:true,
+    minimizer:[
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+      ]		
+  }
 }
