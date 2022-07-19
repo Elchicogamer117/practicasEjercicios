2777f2638007b8658b8d8e5890a44f5c3a7e1feb
Cristian <cris18002011@hotmail.com>
2022-07-18 21:10:04 -0500

    Java poo: creando elementos estáticos

--- Java/POO/MyMedicalAppointments/src/Doctor.java
+++ Java/POO/MyMedicalAppointments/src/Doctor.java
@@ -1,10 +1,12 @@
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
     Doctor(String name){
         System.out.println("El nombre del doctor asignado es: " + name);
@@ -13,5 +15,8 @@ public class Doctor {
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+    public void showid(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- Java/POO/MyMedicalAppointments/src/Main.java
+++ Java/POO/MyMedicalAppointments/src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        //Doctor myDoctor = new Doctor();
-        Doctor myDoctor = new Doctor("Catherine Halsey");
-        myDoctor.showName();
+
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Henry Glassman";
         myDoctor.showName();
+        myDoctor.showid();
+
+        Doctor myDoctorCat = new Doctor();
+        myDoctorCat.showid();
+
+        UIMenu.showMenu();
     }
+
+
 }
