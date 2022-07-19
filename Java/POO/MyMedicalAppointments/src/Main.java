2dd4f7d9d870f9983b8f1dfc4bd9323f705101c4
unknown <cris18002011@hotmail.com>
2022-07-18 20:33:39 -0500

    Java poo: metodo constructor

--- Java/POO/MyMedicalAppointments/src/Main.java
+++ Java/POO/MyMedicalAppointments/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        //Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Catherine Halsey");
+        myDoctor.showName();
         myDoctor.name = "Henry Glassman";
         myDoctor.showName();
     }
