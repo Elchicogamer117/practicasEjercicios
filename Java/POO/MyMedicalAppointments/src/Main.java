70eeeeacb66559c91ca7b9556e64959913bbb641
Cristian <cris18002011@hotmail.com>
2022-07-20 20:53:52 -0500

    Java poo: sobrecarga de metodos constructores

--- Java/POO/MyMedicalAppointments/src/Main.java
+++ Java/POO/MyMedicalAppointments/src/Main.java
@@ -3,7 +3,10 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
+        Doctor mySpartanDoctor = new Doctor("Catherine Elizabeth Halsey","Spartans");
+        mySpartanDoctor.showName();
+        System.out.println(mySpartanDoctor.speciality);
     }
 
 
