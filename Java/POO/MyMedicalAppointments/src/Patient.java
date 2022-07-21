e34b3ca2b0e1e3812de28050033329c61e2bf43a
Cristian <cris18002011@hotmail.com>
2022-07-20 21:02:28 -0500

    Java poo: definiendo clase paciente

---
+++ Java/POO/MyMedicalAppointments/src/Patient.java
@@ -0,0 +1,16 @@
+public class Patient {
+    int id;
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+    public void Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
