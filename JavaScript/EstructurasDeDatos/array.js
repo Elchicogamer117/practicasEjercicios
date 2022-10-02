bd47f18f588ced759a14d5e6f5476bf7e5065323
Cristian <cris18002011@hotmail.com>
2022-10-02 18:37:53 -0500

    JS EDatos: Se agrego validaciones

--- JavaScript/EstructurasDeDatos/array.js
+++ JavaScript/EstructurasDeDatos/array.js
@@ -7,40 +7,69 @@ class MyArray {
         this.data = {};
     }
     get(index) {
-        return this.data[index];
+        if (this.length > 0 && index < this.length) {
+            return this.data[index];
+        } else {
+            return "El indice no existe";
+        }
     }
     push(item) {
-        this.data[this.length] = item;
-        this.length++;
-        return this.data;
+        try {
+            this.data[this.length] = item;
+            this.length++;
+            return this.length;
+        } catch ( error ) {
+            return error;
+        }
+        
     }
     pop() {
-        const lastItem = this.data[this.length - 1];
-        delete this.data[this.length - 1];
-        this.length--;
-        return lastItem;
+        if (this.length > 0) {
+            const lastItem = this.data[this.length - 1];
+            delete this.data[this.length - 1];
+            this.length--;
+            return lastItem;
+        } else {
+            return "Array vacio";
+        }
     }
     delete(index) {
-        const item = this.data[index];
-        for (let i = index; i < this.length - 1; i++) {
-            this.data[i] = this.data[i + 1];
-        }
-        delete this.data[this.length - 1];
-        this.length--;
+        if (index <= this.length) {
+            const item = this.data[index];
+            for (let i = index; i < this.length - 1; i++) {
+                this.data[i] = this.data[i + 1];
+            }
+            delete this.data[this.length - 1];
+            this.length--;
 
-        return item;
+            return item;
+        } else {
+            return "El indice no existe"
+        }
+    }
+    shift() {
+        try {
+            return this.delete(0);
+        } catch ( error ) {
+            return error;
+        }
     }
     unshift(...item) {
-        const newLength = this.length + item.length - 1;
-        for (let i = newLength; i > 0; i--) { // recorremos los elementos existentes
-            this.data[i] = this.data[i-item.length];
+        try {
+            const newLength = this.length + item.length - 1;
+            for (let i = newLength; i > 0; i--) { // recorremos los elementos existentes
+                this.data[i] = this.data[i-item.length];
+            }
+            for(let j = 0; j < item.length; j++) { // agregando nuevos elementos, en las primeras posiciones del "vector"
+                this.data[j] = item[j]
+                this.length++;
+            }
+            return this.length;
+        } catch ( error ) {
+            return error;
         }
-        for(let j = 0; j < item.length; j++) { // agregando nuevos elementos, en las primeras posiciones del "vector"
-            this.data[j] = item[j]
-            this.length++;
-        }
-        return this.data;
     }
+
 
 }
 
