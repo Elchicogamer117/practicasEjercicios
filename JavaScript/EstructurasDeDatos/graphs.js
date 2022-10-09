9e6f70ef3837c204e745f256bbb05dc43754ed45
Cristian <cris18002011@hotmail.com>
2022-10-09 16:29:20 -0500

    JS EDatos: Graphs - Adjacent List

--- JavaScript/EstructurasDeDatos/graphs.js
+++ JavaScript/EstructurasDeDatos/graphs.js
@@ -1,40 +1,43 @@
+class Graph {
+    constructor() {
+        this.nodes = 0;
+        this.adjacentList = {};
+    }
+    addVertex( node ) {
+        this.adjacentList[node] = [];
+        this.nodes++;
+    }
+    addEdge( node1, node2 ) {
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
+}
 
-//    2 - 0
-//   / \
-//  1 - 3
+const myGraph = new Graph();
 
+// Vértices
+myGraph.addVertex(1);
+myGraph.addVertex(3);
+myGraph.addVertex(4);
+myGraph.addVertex(5);
+myGraph.addVertex(6);
+myGraph.addVertex(8);
 
-// Edge List
-const graphEL = [
-    [0, 2],
-    [2, 3],
-    [2, 1],
-    [1, 3],
-];
+// Conexiones
+myGraph.addEdge(8,4)
+myGraph.addEdge(4,1)
+myGraph.addEdge(1,3)
+myGraph.addEdge(1,6)
+myGraph.addEdge(3,6)
+myGraph.addEdge(3,5)
+myGraph.addEdge(5,4)
 
-// Adjacent List
-//                0      1        2        3
-const graphAL = [[2], [2, 3], [0, 1, 3], [1, 2]];
-
-// Object Adjacent List
-const graphOAL = {
-    0: [2],
-    1: [2, 3],
-    2: [0, 1, 3],
-    3: [1, 2],
-};
-
-// Adjacent Matrix
-const graphAM = [
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [1, 1, 0, 1],
-    [0, 1, 1, 0],
-];
-
-const graphOAM = {
-    0: [0, 0, 1, 0],
-    1: [0, 0, 1, 1],
-    2: [1, 1, 0, 1],
-    3: [0, 1, 1, 0],
-};
+/**
+    // RESULTADO:
+    1: (3) [4, 3, 6]
+    3: (3) [1, 6, 5]
+    4: (3) [8, 1, 5]
+    5: (2) [3, 4]
+    6: (2) [1, 3]
+    8: [4] 
+ */
