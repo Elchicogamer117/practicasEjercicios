66890acdc7848606e6291ae5f7f98cb1be5e4d07
Cristian <cris18002011@hotmail.com>
2022-11-01 14:01:11 -0600

    async: Actualización callback

--- asincronismojs/src/callback/challenge.js
+++ asincronismojs/src/callback/challenge.js
@@ -1,34 +1,36 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-let apiRickAndMorty = 'https://rickandmortyapi.com/api/character/';
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const apiRickAndMorty = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(urlAPI, cb){
+function fetchData( urlAPI, callback ) {
     let xhttp = new XMLHttpRequest(); //*Instanciando referencia
     xhttp.open('GET', urlAPI, true); //*? por defecto es true
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState === 4){
-            if (xhttp.status === 200){
-                cb(null, JSON.parse(xhttp.responseText));
+        if ( xhttp.readyState === 4 ) {
+            if ( xhttp.status === 200 ) {
+                callback( null, JSON.parse(xhttp.responseText) );
             } else {
-                const error = new Error('Error ' + urlAPI);
-                return cb(error, null);
+                const error = new Error(`Error: ${urlAPI}`);
+                return callback( error, null );
             }
         }
     }
     xhttp.send();
 }
 
-fetchData(apiRickAndMorty, function(error1, data1){
-    if (error1) return console.error(error1);
-    fetchData(apiRickAndMorty + data1.results[0].id, function(error2, data2){
-        if (error2) return console.error(error2);
-        fetchData(data2.origin.url, function(error3, data3){
-            if (error3) return console.error(error3);
-            console.log(data1.info.count);
-            console.log(data2.name);
-            console.log(data3.dimension);
+fetchData( apiRickAndMorty, ( error1, data1 ) => {
+    if ( error1 ) return console.error(error1);
+
+    fetchData( apiRickAndMorty + data1.results[2].id, ( error2, data2 ) => {
+        if ( error2 ) return console.error(error2);
+
+        fetchData( data2?.origin.url, ( error3, data3 ) => {
+            if ( error3 ) return console.error(error3);
+            console.log(`personajes: ${data1.info.count}`);
+            console.log(`nombre: ${data2.name}`);
+            console.log(`dimensión: ${data3.dimension}`);
 
             console.log(apiRickAndMorty);
-            console.log(apiRickAndMorty + data1.results[0].id); 
+            console.log(apiRickAndMorty + data1.results[1].id); 
             console.log(data2.origin.url); 
         });
     });
--- asincronismojs/src/callback/index.js
+++ asincronismojs/src/callback/index.js
@@ -1,21 +1,34 @@
-function sum(num1, num2){
-    return num1 + num2
+//* --------- Ejemplo 1: Callback directo -------- */
+function suma( num1, num2 ) {
+    return num1 + num2;
 }
-function calc(num1, num2, cb){
-    return cb(num1, num2)
+function resta( num1, num2 ) {
+    return num1 - num2;
 }
-console.log(calc(6,12,sum));
+function calcular( num1, num2, callback ) {
+    return callback( num1, num2 );
+}
+console.log(calcular( 20, 8, suma ));
+console.log(calcular( 20, 8, resta ));
 
-function date(callback){
+//* --------- Ejemplo 2: Callback asincrono -------- */
+
+function date( callback ) {
     console.log(new Date);
     setTimeout(() => {
         let date = new Date
         callback(date)
-    },4000);
+    }, 2000);
 }
-
-function printDate(dateNow){
+function printDate( dateNow ) {
     console.log(dateNow);
 }
-
 date(printDate);
+
+//* --------- Ejemplo 3: Callback asincrono -------- */
+
+function saludo( nombre ) {
+    console.log(`Hola ${nombre} 🖖`);
+}
+
+setTimeout(saludo, 2000, 'Cristian');
